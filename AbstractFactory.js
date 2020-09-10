73d6e7c7476823c2804a1a3f42b8b347407e1fe0
multicalor <d.tretiuk@gmail.com>
2020-09-10 12:06:09 +0300

    Abstract Factory switch

--- AbstractFactory.js
+++ AbstractFactory.js
@@ -1,29 +1,44 @@
 function bmwProduser(kind) {
-    return kind ==='sport'? sportCarFactory: familyCarFactory;
+  switch (kind) {
+    case "sport":
+      return sportCarFactory;
+
+    case "family":
+      return familyCarFactory;
+  }
 }
 
 function sportCarFactory() {
-    return new Z4();
-};
+  return new Z4();
+}
 
 function familyCarFactory() {
-    return new I3();
-};
+  return new I3();
+}
 
 class Z4 {
-    info(){
-        return 'Z4 is a sport car'
-    }
+  info() {
+    return "Z4 is a sport car";
+  }
 }
 
 class I3 {
-    info(){
-        return 'I3 is a family car'
-    }
+  info() {
+    return "I3 is a family car";
+  }
 }
 
-const sportCar = bmwProduser('sport')();
-const familyCar = bmwProduser()();
+const sportCarCreater = bmwProduser("sport");
+const familyCarCreater = bmwProduser("family");
 
-console.log(sportCar.info())
-console.log(familyCar.info())
+const familyCar1 = familyCarCreater();
+const familyCar2 = familyCarCreater();
+
+const sportCar1 = sportCarCreater();
+const sportCar2 = sportCarCreater();
+
+
+console.log(sportCar1.info());
+console.log(sportCar2.info());
+console.log(familyCar1.info());
+console.log(familyCar2.info());
